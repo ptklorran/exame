b7bfd876af5b796a0f08c6dcba209a29d2b077b9
Patrick <ptklorran@gmail.com>
2020-08-25 10:55:18 -0300

    first

---
+++ precache-manifest.09db2145d37dcd941e4d06fd03ac2bb6.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ee1e7a965a4a26ed22f9",
+    "url": "css/app.2c9c8d84.css"
+  },
+  {
+    "revision": "ee1e7a965a4a26ed22f9",
+    "url": "js/app.5e49720a.js"
+  },
+  {
+    "revision": "5044d442d2207104fed4",
+    "url": "css/chunk-vendors.513ea865.css"
+  },
+  {
+    "revision": "5044d442d2207104fed4",
+    "url": "js/chunk-vendors.064942d1.js"
+  },
+  {
+    "revision": "42778c6e7a0bf23ad5e330cbb75daf5f",
+    "url": "index.html"
+  },
+  {
+    "revision": "7476635276e5cefe7ab38b16340bf8ee",
+    "url": "images/icon.png"
+  },
+  {
+    "revision": "735ab4f94fbcd57074377afca324c813",
+    "url": "robots.txt"
+  },
+  {
+    "revision": "7476635276e5cefe7ab38b16340bf8ee",
+    "url": "images/logo.png"
+  }
+];
